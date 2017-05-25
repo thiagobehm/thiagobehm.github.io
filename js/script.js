dcce247392b7aaa3ce89b3f50c45cac080f37f9c
Thiago Luis Vasconcellos Behm <thiagoluisbehm@gmail.com>
2017-05-25 00:21:53 -0300

    update
    update javascript file

--- js/script.js
+++ js/script.js
@@ -1,153 +1,79 @@
-$(document).ready(function () {
-	$("#enviar").bind('click', function () {
-		var nome = $("#name").val();
-		var email = $("#email").val();
-		var telefone = $("#telefone").val();
-		var $mensagem = $("#mensagem").val();
+$(document).ready(function() {
 
-		$.ajax({
-			type: 'POST',
-			url: 'email.php',
-			data: 'name=' + nome + '&email=' + email + '&telefone=' + telefone + '&email=' + email + '&mensagem=' + $mensagem, // NÃO ESQUEÇA DE PASSAR OS VALORES POR AQUI
-			cache: false,
-			success: function (result, status) {}
-		});
-	});
-	$("#telefone").mask("(00) 00000000");
-	var s = skrollr.init();
-});
+  var portfolioPoint = new Waypoint({
+    element: document.getElementById('portfolio'),
+    handler: function() {
+      $("#portfolio").toggleClass("animated tada");
+    }
+  });
 
-$('#myForm').submit(function (e) {
-	$('#mensagemEnviada').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>Mensagem enviada com! <strong>Sucesso...</strong></div>');
-	limpaCampos();
-	resetPlaceholders();
-	e.preventDefault();
+  var panelFormacao = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#panelFormacao").toggleClass("animated bounceInUp");
+    }
+  });
 
-});
+  var yearFormacao = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#yearFormacao").toggleClass("animated bounceInDown");
+    }
+  });
 
-function limpaCampos() {
-	$('#name').val("");
-	$('#email').val("");
-	$('#telefone').val("");
-	$('#mensagem').val("");
-}
+  var panelAndroid = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#panelAndroid").toggleClass("animated fadeInLeft");
+    }
+  });
 
-function resetPlaceholders() {
-	$('#name').attr("placeholder", "nome");
-	$('#email').attr("placeholder", "Email");
-	$('#telefone').attr("placeholder", "Telefone");
-	$('#mensagem').attr("placeholder", "Sua mensagem ou dúvida...");
-}
+  var yearAndroid = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#yearAndroid").toggleClass("animated fadeInRight");
+    }
+  });
 
-var profilePoint = new Waypoint({
-	element: document.getElementById('profile'),
-	handler: function () {
-		$("#profile").toggleClass("animated pulse");
-	}
-});
+  var panelOctalysis = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#panelOctalysis").toggleClass("animated bounceInUp");
+    }
+  });
 
-var portfolioPoint = new Waypoint({
-	element: document.getElementById('portfolio'),
-	handler: function () {
-		$("#portfolio").toggleClass("animated tada");
-	}
-});
+  var yearOctalysis = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#yearOctalysis").toggleClass("animated bounceInDown");
+    }
+  });
 
-var estrategiaPoint = new Waypoint({
-	element: document.getElementById('servicos'),
-	handler: function () {
-		$("#estrategia").toggleClass("animated fadeInLeft");
-	}
-});
-var marcaPoint = new Waypoint({
-	element: document.getElementById('servicos'),
-	handler: function () {
-		$("#marca").toggleClass("animated fadeInDownBig");
-	}
-});
-var webdesignPoint = new Waypoint({
-	element: document.getElementById('servicos'),
-	handler: function () {
-		$("#webdesign").toggleClass("animated fadeInUpBig");
-	}
-});
-var desenvolvimentoPoint = new Waypoint({
-	element: document.getElementById('servicos'),
-	handler: function () {
-		$("#desenvolvimento").toggleClass("animated fadeInRight");
-	}
-});
+  var uxPanel = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#uxPanel").toggleClass("animated fadeInLeft");
+    }
+  });
+  var uxYear = new Waypoint({
+    element: document.getElementById('curriculo'),
+    handler: function() {
+      $("#uxYear").toggleClass("animated fadeInRight");
+    }
+  });
 
-var panelFormacao = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#panelFormacao").toggleClass("animated bounceInUp");
-	}
-});
-var yearFormacao = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#yearFormacao").toggleClass("animated bounceInDown");
-	}
-});
-var panelCertificacao = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#panelCertificacao").toggleClass("animated fadeInRight");
-	}
-});
-var yearItil = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#yearItil").toggleClass("animated fadeInLeft");
-	}
-});
-var panelAndroid = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#panelAndroid").toggleClass("animated fadeInLeft");
-	}
-});
-var yearAndroid = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#yearAndroid").toggleClass("animated fadeInRight");
-	}
-});
-var panelOctalysis = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#panelOctalysis").toggleClass("animated bounceInUp");
-	}
-});
-var yearOctalysis = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#yearOctalysis").toggleClass("animated bounceInDown");
-	}
-});
-var panelDell = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#panelDell").toggleClass("animated bounceInDown");
-	}
-});
-var yearDell = new Waypoint({
-	element: document.getElementById('curriculo'),
-	handler: function () {
-		$("#yearDell").toggleClass("animated bounceInUp");
-	}
-});
-var habilidadesPoint = new Waypoint({
-	element: document.getElementById('habilidades'),
-	handler: function () {
-		$("#skills, #skills2, #skills3, #skills4, #skills5, #skills6, #skills7, #skills8").toggleClass("animated bounceInLeft");
-	}
-});
-var contato = new Waypoint({
-	element: document.getElementById('habilidades'),
-	handler: function () {
-		$("#contato").toggleClass("animated jello");
-	}
+  var habilidadesPointCol = new Waypoint({
+    element: document.getElementById('habilidades'),
+    handler: function() {
+      $("#skillCol").toggleClass("animated bounceInLeft");
+    }
+  });
+
+  var habilidadesPointCol2 = new Waypoint({
+    element: document.getElementById('habilidades'),
+    handler: function() {
+      $("#skillCol2").toggleClass("animated bounceInRight");
+    }
+  });
 
 });
